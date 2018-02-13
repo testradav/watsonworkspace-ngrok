e0c8bbaaa99a234d811f80b2bddd27355d5f393c
Van Staub <van_staub@us.ibm.com>
2018-02-13 10:08:27 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+
+const ngrok = require('ngrok')
+
+ngrok.connect({
+  proto: 'http',
+  addr: process.env.PORT,
+  region: 'us'
+}, (err, url) => {
+  if (err) {
+    console.log(`Error creating ngrok ${err}`)
+  } else {
+    console.log(`
+    1. Open https://developer.watsonwork.ibm.com/apps/${process.env.APP_ID}/info 
+    2. Add the webhook ${url}/${process.env.APP_ID}/webhook to you App
+    `)
+  }
+})
